4c39f353f8f081811a8f10175b54a551e0cc46f1
C4mill3 <C4mill3>
2025-09-28 12:22:12 +0200

    removing unused file for server

--- tailwindcss/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## How to: https://github.com/tailwindlabs/tailwindcss/discussions/15855
-
-## exec install https://tailwindcss.com/docs/installation/tailwind-cli
-
-./tailwindcss -i input.css -o ../static/css/tailwind.css --watch
--- tailwindcss/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['/home/cam/Desktop/selfcert-manager/templates/**/*.html.j2'], // Chemin vers vos fichiers HTML
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
