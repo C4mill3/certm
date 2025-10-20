08703acaf1499ab64547d125af44970f012a2cdc
C4mill3 <C4mill3>
2025-10-20 18:58:54 +0200

    adding front

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,67 @@
 mod tools;
 
+use std::io;
+use std::rc::Rc;
+use crossterm::{
+    event,
+    terminal,
+};
 
-fn main(){
-    let b = tools::certs_manager::generate_new_ca(4096, "test", "ESIEA", "FR");
-    let a = tools::certs_manager::list_ca();
-    println!("res1 {:?}", b);
-    println!("res: {:?}", a);
-    let c = tools::certs_manager::generate_new_cert("test", 4096, "test.com", "ESIEA", "FR");
-    println!("ress : {:?}", c);
-    let d = tools::certs_manager::list_cert("test");
-    println!("ress : {:?}", d);
+pub fn run_menu() -> Result<(), Box<dyn std::error::Error>> {
+    terminal::enable_raw_mode()?;
+    let mut stdout = io::stdout();
 
-    return;
-} 
+    // Define some example actions wrapped in Rc
+    let action1 = Rc::new(|| {
+        println!("Action 3 executed!");
+        println!("Additional logic for Action 3");
+        let _ = event::read(); // Wait for any key press
+    });
+
+    let sub_action1 = Rc::new(|| println!("Sub-action 1 executed!"));
+
+    let sub_action2 = Rc::new(|| println!("Sub-action 2 executed!"));
+
+
+    let submenu2 = tools::front::Menu::new(
+        "Menu / Submenu 1 / Submenu 2",
+        vec![
+            tools::front::MenuItem::Action("sub-choice 1", sub_action1.clone()),
+            tools::front::MenuItem::Action("sub-choice 2", sub_action2.clone()),
+        ],
+    );
+
+
+    // Create submenu for Choice 2
+    let submenu = tools::front::Menu::new(
+        "Menu / Submenu 1",
+        vec![
+            tools::front::MenuItem::Submenu("sub-choice 1", submenu2),
+        ],
+    );
+
+    // Create main menu
+    let mut main_menu = tools::front::Menu::new(
+        "Menu",
+        vec![
+            tools::front::MenuItem::Action("List", action1.clone()),
+            tools::front::MenuItem::Submenu("Create", submenu),
+            tools::front::MenuItem::Action("Import", action1.clone()),
+            tools::front::MenuItem::Action("Export", action1.clone()),
+        ],
+    );
+
+    // Run the menu system
+    main_menu.run(&mut stdout)?;
+    
+    // Disable raw mode
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run_menu() {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
+}
--- src/tools.rs
+++ src/tools.rs
@@ -68,7 +68,6 @@ mod utility{
         }
     }
     
-    
 
 }    
 
@@ -253,4 +252,108 @@ pub mod certs_manager{
         Ok(true)
     }
 
+}
+
+
+pub mod front {
+    use std::io::{self, Write};
+    use std::rc::Rc;
+    use crossterm::{
+        cursor,
+        event::{self, Event, KeyCode, KeyEvent},
+        execute,
+        terminal::{self, ClearType},
+        ExecutableCommand,
+    };
+
+    pub enum MenuItem {
+        Action(&'static str, Rc<dyn Fn()>),
+        Submenu(&'static str, Menu),
+    }
+
+    pub struct Menu {
+        pub title: &'static str,
+        pub items: Vec<MenuItem>,
+        pub current_index: usize,
+    }
+
+    impl Menu {
+        pub fn new(title: &'static str, items: Vec<MenuItem>) -> Self {
+            Self {
+                title,
+                items,
+                current_index: 0,
+            }
+        }
+
+        pub fn run(&mut self, stdout: &mut io::Stdout) -> Result<(), Box<dyn std::error::Error>> {
+            loop {
+                self.display(stdout)?;
+
+                if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+                    match code {
+                        KeyCode::Up => {
+                            if self.current_index > 0 {
+                                self.current_index -= 1;
+                            }
+                        }
+                        KeyCode::Down => {
+                            if self.current_index < self.items.len() - 1 {
+                                self.current_index += 1;
+                            }
+                        }
+                        KeyCode::Enter => {
+                            match &self.items[self.current_index] {
+                                MenuItem::Action(_, action) => {
+                                    // Clear screen and execute action
+                                    execute!(stdout, terminal::Clear(ClearType::All), cursor::MoveTo(0, 0), cursor::Hide)?;
+                                    // Clone the Rc to get ownership and call the closure
+                                    let action_cloned = action.clone();
+                                    (&*action_cloned)();
+                                    return Ok(()); // end action
+                                }
+                                MenuItem::Submenu(_, submenu) => {
+                                    // Run submenu
+                                    let mut sub = submenu.clone();
+                                    sub.run(stdout)?;
+                                }
+                            }
+                        }
+                        KeyCode::Esc | KeyCode::Char('q') => return Ok(()), // Exit
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        fn display(&self, stdout: &mut io::Stdout) -> Result<(), Box<dyn std::error::Error>> {
+            execute!(stdout, terminal::Clear(ClearType::All), cursor::MoveTo(0, 0), cursor::Hide)?;
+            write!(stdout, "{}\r\n", self.title)?;
+            for (i, item) in self.items.iter().enumerate() {
+                let prefix = if i == self.current_index { "\x1b[1m> " } else { "  " };
+                write!(stdout, "{}", prefix)?;
+                match item {
+                    MenuItem::Action(name, _) => write!(stdout, "{}", name)?,
+                    MenuItem::Submenu(name, _) => write!(stdout, "{}", name)?,
+                }
+                write!(stdout, "\x1b[0m\r\n")?; // reset + new line
+            }
+            stdout.flush()?;
+            Ok(())
+        }
+    }
+
+    impl Clone for Menu {
+        fn clone(&self) -> Self {
+            // Simple clone; actions are stored in Rc so they can be cloned cheaply
+            Self {
+                title: self.title,
+                items: self.items.iter().map(|item| match item {
+                    MenuItem::Action(name, action) => MenuItem::Action(name, action.clone()),
+                    MenuItem::Submenu(name, submenu) => MenuItem::Submenu(name, submenu.clone()),
+                }).collect(),
+                current_index: self.current_index,
+            }
+        }
+    }
 }
